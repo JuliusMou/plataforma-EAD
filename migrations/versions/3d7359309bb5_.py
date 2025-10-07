796bce40c17f25ed732555eb3f1111fe2bc80079
julio <juliusgomes89@hotmail.com>
2025-10-06 22:14:17 -0300

    Implementa os cards dos cursos em andamentos

---
+++ migrations/versions/3d7359309bb5_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3d7359309bb5
+Revises: 1d533bd04308, f91feed41500
+Create Date: 2025-10-06 21:37:59.802179
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3d7359309bb5'
+down_revision = ('1d533bd04308', 'f91feed41500')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
